304ce83bc2bb10df7f9b6591f0fbb3bc144f3a54
JamesA-Hunter <JamesA.Hunter@outlook.com>
2024-03-26 07:42:43 +0000

    ready up button created, needs conditional statement adding to room.js and button logic + server logic

---
+++ client/src/components/ReadyUpBtn.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ReadyUpBtn(props) {
+
+    return(
+    <>
+        <label className=""></label>
+        <div className="space-x-1 rounded bg-blue-300 hover:bg-blue-600">
+        <button className="px-2 text-center">Ready Up</button>
+        </div>
+    </>
+    );
+}
+
+export default ReadyUpBtn;
